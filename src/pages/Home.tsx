4c52873b702fcae6488819f837560688e53f405f
Leandro Piñeiro <leandro.kondratzky@gmail.com>
2025-05-11 19:18:25 -0300

    feat: zustand

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { useCounter } from "@/contexts/counter";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
 
 export const HomePage = () => {
+  const { count, inc } = useCounter();
+
   return (
     <Box p="4">
       <Heading>Bienvenido</Heading>
       <Text>Esta es la página de inicio.</Text>
+      <Text>Contador: {count}</Text>
+      <Button onClick={inc}>Incrementar</Button>
     </Box>
   );
 };

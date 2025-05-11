b15c97b136b53415d5706c6055c58cbb6bd52947
Leandro Piñeiro <leandro.kondratzky@gmail.com>
2025-05-11 18:37:33 -0300

    feat: react-router v7

---
+++ src/router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import { HomePage } from "./pages/Home";
+import { AboutPage } from "./pages/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+]);
+
+export default router;
